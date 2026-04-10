ffe8fda30de5f2d3513b4fd259d8eb9a504ec0e4
Naufalag <naufal.abdurrahman.gumelar@gmail.com>
2026-04-11 00:20:36 +0700

    FIX

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'naufal-v10';
+const assets = ['./', './index.html', './style.css', './script.js', './logo.jpg', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(assets)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keys) => Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+    )));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
